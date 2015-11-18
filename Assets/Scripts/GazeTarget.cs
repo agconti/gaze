6f0a27f17cb191e39df0662a2e38609f502384a4
agconti <agc11d@gmail.com>
2015-11-18 12:37:20 -0500

    feat(GazeInput): basic event system WIP

---
+++ Assets/Scripts/GazeTarget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+
+public class GazeTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter (PointerEventData eventData) {
+		Debug.LogFormat ("Entered Gaze? {0}", eventData);
+	}
+
+	public void OnPointerExit (PointerEventData eventData) {
+		Debug.LogFormat ("Left Gaze? {0}", eventData);
+	}
+}
